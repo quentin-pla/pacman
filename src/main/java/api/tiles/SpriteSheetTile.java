77312cd23ab8a001d69c91ee3b7e19da158bd223
Quentin Pla <quentin-pla@hotmail.fr>
2020-10-27 16:44:36 +0100

    AJout de la classe SpriteSheetTile pour générer des carreaux à partir de fichier de texture

---
+++ src/main/java/api/tiles/SpriteSheetTile.java
@@ -0,0 +1,81 @@
+package api.tiles;
+
+import api.textures.SpriteSheet;
+
+import static org.lwjgl.opengl.GL11.*;
+
+/**
+ * Carreau affiché avec un fichier de textures
+ */
+public class SpriteSheetTile extends Tile {
+    /**
+     * Fichier de textures
+     */
+    private SpriteSheet spriteSheet;
+
+    /**
+     * Coordonnée horizontale de la texture
+     */
+    private int x_coord;
+
+    /**
+     * Coordonnée verticale de la texture
+     */
+    private int y_coord;
+
+    /**
+     * Constructeur
+     * @param size dimensions
+     * @param spriteSheet fichier de textures
+     */
+    public SpriteSheetTile(int size, SpriteSheet spriteSheet, int x_coord, int y_coord) {
+        super(size);
+        this.spriteSheet = spriteSheet;
+        this.x_coord = x_coord - 1;
+        this.y_coord = y_coord - 1;
+    }
+
+    @Override
+    public void render(int x, int y) {
+        this.x = x;
+        this.y = y;
+        spriteSheet.getTexture().bind();
+        glBegin(GL_QUADS);
+        glTexCoord2f((x_coord + 0f) / spriteSheet.getWidth(),(y_coord + 0f) / spriteSheet.getHeight());
+        glVertex2i(x, y);
+        glTexCoord2f((x_coord + 1f) / spriteSheet.getWidth(),(y_coord + 0f) / spriteSheet.getHeight());
+        glVertex2i(x + size, y);
+        glTexCoord2f((x_coord + 1f) / spriteSheet.getWidth(),(y_coord + 1f) / spriteSheet.getHeight());
+        glVertex2i(x + size, y + size);
+        glTexCoord2f((x_coord + 0f) / spriteSheet.getWidth(),(y_coord + 1f) / spriteSheet.getHeight());
+        glVertex2i(x, y + size);
+        glEnd();
+        spriteSheet.getTexture().unbind();
+    }
+
+    // GETTERS & SETTERS //
+
+    public SpriteSheet getSpriteSheet() {
+        return spriteSheet;
+    }
+
+    public void setSpriteSheet(SpriteSheet spriteSheet) {
+        this.spriteSheet = spriteSheet;
+    }
+
+    public int getX_coord() {
+        return x_coord;
+    }
+
+    public void setX_coord(int x_coord) {
+        this.x_coord = x_coord;
+    }
+
+    public int getY_coord() {
+        return y_coord;
+    }
+
+    public void setY_coord(int y_coord) {
+        this.y_coord = y_coord;
+    }
+}
