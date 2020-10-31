1f74f39de98101b36c069d513333dff79a233438
Quentin Pla <quentin-pla@hotmail.fr>
2020-10-31 18:32:02 +0100

    Ajout des attributs hauteur et largeur pour les entités + maj constructeur + maj fonctions

--- src/main/java/engines/graphics/Entity.java
+++ src/main/java/engines/graphics/Entity.java
@@ -12,6 +12,16 @@ public abstract class Entity extends Renderer {
     protected Scene scene;
 
     /**
+     * Hateur en pixels
+     */
+    protected int height;
+
+    /**
+     * Largeur en pixels
+     */
+    protected int width;
+
+    /**
      * Position horizontale
      */
     protected int x;
@@ -22,19 +32,38 @@ public abstract class Entity extends Renderer {
     protected int y;
 
     /**
-     * Constructeur
+     * Constructeur avec dimensions
+     * @param height hauteur
+     * @param width largeur
      * @param x position horizontale
      * @param y position verticale
      */
-    protected Entity(int x, int y) {
+    protected Entity(int height, int width, int x, int y) {
+        this.height = height;
+        this.width = width;
         this.x = x;
         this.y = y;
+    }
+
+    /**
+     * Constructeur avec taille
+     * @param size taille
+     * @param x position horizontale
+     * @param y position verticale
+     */
+    protected Entity(int size, int x, int y) {
+        this(size, size, x, y);
     }
 
     /**
      * Dessiner l'entité
      */
     protected abstract void draw();
+
+    /**
+     * Mettre à jour l'entité
+     */
+    protected abstract void update();
 
     /**
      * Effacer l'entité
@@ -45,12 +74,40 @@ public abstract class Entity extends Renderer {
 
     /**
      * Translater l'entité
-     * @param x position horizontale
-     * @param y position verticale
+     * @param x nombre à additionner à la position horizontale
+     * @param y nombre à additionner à la position verticale
      */
     protected void translate(int x, int y) {
+        this.x = this.x + x;
+        this.y = this.y + y;
+    }
+
+    /**
+     * Déplacer l'entité
+     * @param x nouvelle position horizontale
+     * @param y nouvelle position verticale
+     */
+    protected void move(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    /**
+     * Redimensionner l'entité
+     * @param height hauteur
+     * @param width largeur
+     */
+    protected void resize(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    /**
+     * Redimensionner l'entité
+     * @param size taille
+     */
+    protected void resize(int size) {
+        resize(size, size);
     }
 
     // GETTERS & SETTERS //
@@ -61,6 +118,22 @@ public abstract class Entity extends Renderer {
 
     protected void setScene(Scene scene) {
         this.scene = scene;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    protected void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    protected void setWidth(int width) {
+        this.width = width;
     }
 
     public int getX() {
