591b777283b9329b2725ac94155c8402c810a2dc
Quentin Pla <quentin-pla@hotmail.fr>
2020-10-31 18:38:54 +0100

    Modification de la classe Level pour prendre en compte une liste d'objets présents sur celle-ci + ajout fonctions

--- src/main/java/application/Level.java
+++ src/main/java/application/Level.java
@@ -1,16 +1,24 @@
 package application;
 
+import engines.graphics.Scene;
 import engines.graphics.Tile;
 import engines.graphics.TilesMatrix;
+
+import java.util.ArrayList;
 
 /**
  * Niveau de jeu
  */
-public class Level {
+public class Level extends TilesMatrix {
     /**
-     * Matrice
+     * Objets présents sur le niveau
      */
-    private TilesMatrix matrix;
+    private ArrayList<GameObject> game_objects = new ArrayList<>();
+
+    /**
+     * Joueur
+     */
+    private Player player;
 
     /**
      * Chronomètre
@@ -18,13 +26,77 @@ public class Level {
     private float timer;
 
     /**
-     * Constructeur
-     * @param height hauteur
-     * @param width largeur
-     * @param max_time temps maximum
+     * Score minimum à atteindre pour gagner
      */
-    public Level(int height, int width, int max_time) {
-        this.matrix = new TilesMatrix(height, width, new Tile(30));
-        this.timer = max_time;
+    private int minimum_score;
+
+    /**
+     * Score actuel
+     */
+    private int actual_score;
+
+    /**
+     * Constructeur
+     * @param rows nombre de lignes
+     * @param cols nombre de colonnes
+     */
+    public Level(int rows, int cols) {
+        super(rows,cols, new Tile(30));
     }
+
+    /**
+     * Constructeur surchargé
+     * @param rows nombre de lignes
+     * @param cols nombre de colonnes
+     * @param floor sol par défaut
+     */
+    public Level(int rows, int cols, Tile floor) {
+        super(rows,cols, floor);
+    }
+
+    /**
+     * Faire apparaitre le joueur à un endroit
+     * @param player joueur
+     * @param row ligne
+     * @param col colonne
+     */
+    public void addPlayer(Player player, int row, int col) {
+        player.addMoveBounds(getBounds());
+        Tile tile = get(row-1,col-1);
+        player.teleport(tile.getX(), tile.getY());
+        this.player = player;
+        game_objects.add(player);
+    }
+
+    @Override
+    protected void setScene(Scene scene) {
+        super.setScene(scene);
+        //Ajout des objets du niveau à la scène
+        for (GameObject object : game_objects)
+            scene.addEntity(object, object.getX(), object.getY());
+    }
+
+    @Override
+    protected void translate(int x, int y) {
+        super.translate(x,y);
+        //Translation des objets du niveau
+        for (GameObject object : game_objects) object.translate(x,y);
+        //Mise à jour des limites de déplacement pour le joueur
+        if (player != null) player.addMoveBounds(getBounds());
+    }
+
+    @Override
+    protected void move(int x, int y) {
+        super.move(x,y);
+        //Translation des objets du niveau
+        for (GameObject object : game_objects) object.translate(x,y);
+        //Mise à jour des limites de déplacement pour le joueur
+        if (player != null) player.addMoveBounds(getBounds());
+    }
+
+    /**
+     * Récupérer les limites du niveau
+     * @return limites
+     */
+    public int[] getBounds() { return new int[]{x, y, x + width, y + height}; }
 }
