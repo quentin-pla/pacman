91311eb6d239e1224a6844b1de31076ec94121d1
Quentin Pla <quentin-pla@hotmail.fr>
2020-11-02 16:21:38 +0100

    Modification déplacements comme vrai pacman

--- src/main/java/gameplay/Player.java
+++ src/main/java/gameplay/Player.java
@@ -7,8 +7,7 @@ import engines.graphics.EntityTexture;
 import java.util.HashMap;
 import java.util.Map;
 
-import static engines.input_output.InputOutputEngine.getPressedKeys;
-import static engines.input_output.InputOutputEngine.isKeyPressed;
+import static engines.input_output.InputOutputEngine.*;
 import static org.lwjgl.glfw.GLFW.*;
 
 /**
@@ -21,12 +20,6 @@ public class Player extends Entity {
     public enum MoveDirection {
         UP,RIGHT,DOWN,LEFT
     }
-
-    /**
-     * Directions autorisées déplacement
-     * dans le sens des aiguilles d'une montre
-     */
-    private boolean[] allowed_moves = new boolean[]{true,true,true,true};
 
     /**
      * Vitesse de déplacement (en pixels)
@@ -63,17 +56,6 @@ public class Player extends Entity {
     }
 
     /**
-     * Changer la manière de déplacer le joueur
-     * @param up vers le haut
-     * @param right vers la droite
-     * @param down vers le bas
-     * @param left vers la gauche
-     */
-    public void allowMoveDirections(boolean up, boolean right, boolean down, boolean left) {
-        allowed_moves = new boolean[]{up,right,down,left};
-    }
-
-    /**
      * Ajouter des limites de déplacement
      * @param bounds limites
      */
@@ -94,36 +76,26 @@ public class Player extends Entity {
      * Déplacer le joueur
      */
     private void movePlayer() {
-        //Déplacement du joueur
-        if (allowed_moves[0] && isKeyPressed(GLFW_KEY_UP))
-            if (move_bounds == null || y > move_bounds[1]) y -= move_speed;
-        if (allowed_moves[1] && isKeyPressed(GLFW_KEY_RIGHT))
-            if (move_bounds == null || x < move_bounds[2]) x += move_speed;
-        if (allowed_moves[2] && isKeyPressed(GLFW_KEY_DOWN))
-            if (move_bounds == null || y < move_bounds[3]) y += move_speed;
-        if (allowed_moves[3] && isKeyPressed(GLFW_KEY_LEFT))
-            if (move_bounds == null || x > move_bounds[0]) x -= move_speed;
-        //Ajout de l'animation liée au déplacement
-        if (!getPressedKeys().isEmpty()) {
-            switch (getPressedKeys().get(getPressedKeys().size() - 1)) {
-                case GLFW_KEY_UP:
-                    bindTexture(animations.get(MoveDirection.UP.name()));
-                    break;
-                case GLFW_KEY_RIGHT:
-                    bindTexture(animations.get(MoveDirection.RIGHT.name()));
-                    break;
-                case GLFW_KEY_DOWN:
-                    bindTexture(animations.get(MoveDirection.DOWN.name()));
-                    break;
-                case GLFW_KEY_LEFT:
-                    bindTexture(animations.get(MoveDirection.LEFT.name()));
-                    break;
-                default:
-                    bindTexture(default_texture);
-                    break;
-            }
-        } else {
-            bindTexture(default_texture);
+        switch (getLastPressedKey()) {
+            case GLFW_KEY_UP:
+                if (move_bounds == null || y > move_bounds[1]) y -= move_speed;
+                bindTexture(animations.get(MoveDirection.UP.name()));
+                break;
+            case GLFW_KEY_RIGHT:
+                if (move_bounds == null || x < move_bounds[2]) x += move_speed;
+                bindTexture(animations.get(MoveDirection.RIGHT.name()));
+                break;
+            case GLFW_KEY_DOWN:
+                if (move_bounds == null || y < move_bounds[3]) y += move_speed;
+                bindTexture(animations.get(MoveDirection.DOWN.name()));
+                break;
+            case GLFW_KEY_LEFT:
+                if (move_bounds == null || x > move_bounds[0]) x -= move_speed;
+                bindTexture(animations.get(MoveDirection.LEFT.name()));
+                break;
+            default:
+                bindTexture(default_texture);
+                break;
         }
     }
 
