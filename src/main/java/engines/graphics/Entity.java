75f4fd780a2f302e22f19fa8be6e48841712141c
Quentin Pla <quentin-pla@hotmail.fr>
2020-11-06 21:29:25 +0100

    Séparation de la classe GraphicsEntity en une classe et une interface -> Entity pour les attributs -> GraphicsEntity pour les méthodes

--- src/main/java/engines/graphics/Entity.java
+++ src/main/java/engines/graphics/Entity.java
@@ -1,11 +1,9 @@
 package engines.graphics;
 
-import api.SwingRenderer;
-
 /**
- * Entité
+ * Entité graphique
  */
-public class Entity extends SwingRenderer {
+public class Entity implements GraphicsEntity {
     /**
      * Scène
      */
@@ -63,118 +61,86 @@ public class Entity extends SwingRenderer {
         this.y = 0;
         this.scene = null;
         this.color = clone.color;
-        this.texture = clone.texture.clone();
+        if (clone.texture != null)
+            this.texture = clone.texture.clone();
     }
 
-    /**
-     * Cloner une entité
-     * @return clone
-     */
+    @Override
     public Entity clone() {
         return new Entity(this);
     }
 
-    /**
-     * Dessiner l'entité
-     */
+    @Override
     public void draw() {
         if (color != null) color.cover(this);
         if (texture != null) texture.cover(this);
     }
 
-    /**
-     * Mettre à jour l'entité
-     */
+    @Override
     public void update() {
         if (texture != null) texture.update();
     }
 
-    /**
-     * Effacer l'entité
-     */
+    @Override
     public void erase() {
         scene.removeEntity(this);
     }
 
-    /**
-     * Translater l'entité
-     * @param x nombre à additionner à la position horizontale
-     * @param y nombre à additionner à la position verticale
-     */
+    @Override
     public void translate(int x, int y) {
         this.x = this.x + x;
         this.y = this.y + y;
     }
 
-    /**
-     * Déplacer l'entité
-     * @param x nouvelle position horizontale
-     * @param y nouvelle position verticale
-     */
+    @Override
     public void move(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    /**
-     * Redimensionner l'entité
-     * @param height hauteur
-     * @param width largeur
-     */
+    @Override
     public void resize(int height, int width) {
         this.height = height;
         this.width = width;
     }
 
-    /**
-     * Attacher une couleur
-     * @param red intensité rouge
-     * @param green intensité vert
-     * @param blue intensité bleu
-     */
+    @Override
     public void bindColor(int red, int green, int blue) {
         color = new Color(red, green, blue);
     }
 
-    /**
-     * Détacher la couleur
-     */
+    @Override
     public void unbindColor() { color = null; }
 
-    /**
-     * Attacher une texture
-     * @param texture texture
-     */
-    public void bindTexture(EntityTexture texture) { this.texture = texture; }
+    @Override
+    public void bindTexture(Cover texture) { this.texture = texture; }
 
-    /**
-     * Détacher la texture
-     */
+    @Override
     public void unbindTexture() { texture = null; }
 
     // GETTERS & SETTERS //
 
+    @Override
     public Scene getScene() { return scene; }
 
-    protected void setScene(Scene scene) { this.scene = scene; }
+    @Override
+    public void setScene(Scene scene) { this.scene = scene; }
 
+    @Override
     public int getHeight() { return height; }
 
-    public void setHeight(int height) { this.height = height; }
-
+    @Override
     public int getWidth() { return width; }
 
-    public void setWidth(int width) { this.width = width; }
-
+    @Override
     public int getX() { return x; }
 
-    public void setX(int x) { this.x = x; }
-
+    @Override
     public int getY() { return y; }
 
-    public void setY(int y) { this.y = y; }
-
+    @Override
     public Color getColor() { return color; }
 
-    public EntityTexture getTexture() { return texture; }
+    @Override
+    public Cover getTexture() { return texture; }
 }
---
+++ src/main/java/engines/graphics/GraphicsEntity.java
@@ -0,0 +1,90 @@
+package engines.graphics;
+
+/**
+ * Entité graphique
+ */
+public interface GraphicsEntity {
+    /**
+     * Dessiner l'entité
+     */
+    void draw();
+
+    /**
+     * Mettre à jour l'entité
+     */
+    void update();
+
+    /**
+     * Effacer l'entité
+     */
+    void erase();
+
+    /**
+     * Translater l'entité
+     * @param x nombre à additionner à la position horizontale
+     * @param y nombre à additionner à la position verticale
+     */
+    void translate(int x, int y);
+
+    /**
+     * Déplacer l'entité
+     * @param x nouvelle position horizontale
+     * @param y nouvelle position verticale
+     */
+    void move(int x, int y);
+
+    /**
+     * Redimensionner l'entité
+     * @param height hauteur
+     * @param width largeur
+     */
+    void resize(int height, int width);
+
+    /**
+     * Attacher une couleur
+     * @param red intensité rouge
+     * @param green intensité vert
+     * @param blue intensité bleu
+     */
+    void bindColor(int red, int green, int blue);
+
+    /**
+     * Détacher la couleur
+     */
+    void unbindColor();
+
+    /**
+     * Attacher une texture
+     * @param texture texture
+     */
+    void bindTexture(Cover texture);
+
+    /**
+     * Détacher la texture
+     */
+    void unbindTexture();
+
+    /**
+     * Cloner l'entité
+     * @return clone
+     */
+    GraphicsEntity clone();
+
+    // GETTERS & SETTERS //
+
+    Scene getScene();
+
+    void setScene(Scene scene);
+
+    int getHeight();
+
+    int getWidth();
+
+    int getX();
+
+    int getY();
+
+    Color getColor();
+
+    Cover getTexture();
+}
