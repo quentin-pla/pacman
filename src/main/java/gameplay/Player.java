860c9c225e99d7e58e2a24b169316410cd6ff0dd
Quentin Pla <quentin-pla@hotmail.fr>
2020-11-06 21:38:07 +0100

    Mise à jour de la classe Player en étandant la classe Entity

--- src/main/java/gameplay/Player.java
+++ src/main/java/gameplay/Player.java
@@ -1,14 +1,12 @@
 package gameplay;
 
-import engines.graphics.Entity;
-import engines.graphics.EntityTexture;
+import engines.graphics.Cover;
 import engines.graphics.SpriteAnimation;
+import engines.kernel.Entity;
 
 import java.awt.event.KeyEvent;
 import java.util.HashMap;
 import java.util.Map;
-
-import static engines.input_output.KeyboardInputOutput.getLastPressedKey;
 
 /**
  * Joueur
@@ -35,7 +33,7 @@ public class Player extends Entity {
     /**
      * Texture par défaut
      */
-    private EntityTexture default_texture;
+    private Cover default_texture;
 
     /**
      * Animations
@@ -49,8 +47,10 @@ public class Player extends Entity {
      * @param move_speed vitesse de déplacement
      * @param default_texture texture par défaut
      */
-    public Player(int height, int width, int move_speed, EntityTexture default_texture) {
-        super(height, width);
+    public Player(int height, int width, int move_speed, Cover default_texture) {
+        this.initGraphics(height, width);
+        this.initInputOutput();
+        this.getInputOutput().enableKeyboardInputOutput(true);
         this.move_speed = move_speed;
         this.default_texture = default_texture;
     }
@@ -76,21 +76,25 @@ public class Player extends Entity {
      * Déplacer le joueur
      */
     private void movePlayer() {
-        switch (getLastPressedKey()) {
+        switch (getKeyboard().getLastPressedKey()) {
             case KeyEvent.VK_UP:
-                if (move_bounds == null || y > move_bounds[1]) y -= move_speed;
+                if (move_bounds == null || getY() > move_bounds[1])
+                    translate(0, -move_speed);
                 bindTexture(animations.get(MoveDirection.UP.name()));
                 break;
             case KeyEvent.VK_RIGHT:
-                if (move_bounds == null || x < move_bounds[2]) x += move_speed;
+                if (move_bounds == null || getX() < move_bounds[2])
+                    translate(move_speed, 0);
                 bindTexture(animations.get(MoveDirection.RIGHT.name()));
                 break;
             case KeyEvent.VK_DOWN:
-                if (move_bounds == null || y < move_bounds[3]) y += move_speed;
+                if (move_bounds == null || getY() < move_bounds[3])
+                    translate(0, move_speed);
                 bindTexture(animations.get(MoveDirection.DOWN.name()));
                 break;
             case KeyEvent.VK_LEFT:
-                if (move_bounds == null || x > move_bounds[0]) x -= move_speed;
+                if (move_bounds == null || getX() > move_bounds[0])
+                    translate(-move_speed, 0);
                 bindTexture(animations.get(MoveDirection.LEFT.name()));
                 break;
             default:
