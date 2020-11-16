a99700ebac79cb90dde9a385a1a363d602ef15cf
Quentin Pla <quentin-pla@hotmail.fr>
2020-11-16 13:46:40 +0100

    MAJ main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,17 +9,21 @@ import gameplay.Player;
 public class Main {
     public static void main(String[] args) {
         //Transfert des textures
-        int spriteSheet = GraphicsEngine.loadSpriteSheet("sprite_sheet.png", 11, 11);
+        int spriteSheet = GraphicsEngine.loadSpriteSheet("assets/sprite_sheet.png", 11, 11);
 
+        //Joueur
         Player player = new Player(30, 30, 2, spriteSheet, 1, 3);
+        PhysicsEngine.move(player.getEntityID(),0,0);
 
+        //Objet simple
         int object = KernelEngine.generateEntity();
-        PhysicsEngine.move(object,135,115);
+        PhysicsEngine.move(object,135,135);
         GraphicsEngine.resize(object,30,30);
         GraphicsEngine.bindColor(object,255,0,0);
 
         //Ajout des collisions
-        //PhysicsEngine.addCollisions(player.getEntityID(), object);
+        PhysicsEngine.addCollisions(player.getEntityID(), object);
+        PhysicsEngine.addBoundLimits(player.getEntityID(), 0,0,300,300);
 
         //Scène principale
         int mainScene = GraphicsEngine.generateScene(300, 300);
@@ -30,6 +34,5 @@ public class Main {
 
         //Démarrage du jeu
         KernelEngine.start();
-
     }
 }
