51ee90a44ab09be44cdb3cd58c1806c89f1d2495
Quentin Pla <quentin-pla@hotmail.fr>
2020-11-18 00:09:47 +0100

    Création d'un mini test pour vérifier les collisions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,10 @@
-import engines.graphics.GraphicsEngine;
-import engines.kernel.KernelEngine;
-import engines.physics.PhysicsEngine;
-import gameplay.Player;
+import gameplay.Gameplay;
 
 /**
  * Main
  */
 public class Main {
     public static void main(String[] args) {
-        //Transfert des textures
-        int spriteSheet = GraphicsEngine.loadSpriteSheet("assets/sprite_sheet.png", 11, 11);
-
-        //Joueur
-        Player player = new Player(30, 30, 2, spriteSheet, 1, 3);
-        PhysicsEngine.move(player.getEntityID(),0,0);
-
-        //Objet simple
-        int object = KernelEngine.generateEntity();
-        PhysicsEngine.move(object,135,135);
-        GraphicsEngine.resize(object,30,30);
-        GraphicsEngine.bindColor(object,255,0,0);
-
-        //Ajout des collisions
-        PhysicsEngine.addCollisions(player.getEntityID(), object);
-        PhysicsEngine.addBoundLimits(player.getEntityID(), 0,0,300,300);
-
-        //Scène principale
-        int mainScene = GraphicsEngine.generateScene(300, 300);
-        GraphicsEngine.setSceneBackgroundColor(mainScene,0,0,0);
-        GraphicsEngine.bindScene(mainScene);
-        GraphicsEngine.addToCurrentScene(object);
-        GraphicsEngine.addToCurrentScene(player.getEntityID());
-
-        //Démarrage du jeu
-        KernelEngine.start();
+        new Gameplay();
     }
 }
--- src/main/java/gameplay/Gameplay.java
+++ src/main/java/gameplay/Gameplay.java
@@ -6,129 +6,92 @@ import engines.kernel.Event;
 import engines.kernel.KernelEngine;
 import engines.physics.PhysicsEngine;
 
-import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
+/**
+ * Gameplay
+ */
 public class Gameplay {
+    /**
+     * Identifiant du fichier contenant les textures du jeu
+     */
+    private static final int textures = GraphicsEngine.loadSpriteSheet("assets/sprite_sheet.png", 11, 11);
 
-    static int spriteSheet = GraphicsEngine.loadSpriteSheet("assets/sprite_sheet.png", 11, 11);
-    static Player player = new Player(30, 30, 2, spriteSheet, 1, 3);
+    /**
+     * Joueur
+     */
+    private static Player player;
 
-
-    enum EVENTS {
-        GoUp(new Event() {
-            @Override
-            public void run() {
-
-                PhysicsEngine.goUp(player.getEntityID(), player.getMoveSpeed());
-                GraphicsEngine.bindAnimation(player.getEntityID(), player.getAnimations().get(Player.MoveDirection.UP.name()));
-            }
-        }),
-
-        GoDown(new Event() {
-            @Override
-            public void run() {
-
-                PhysicsEngine.goDown(player.getEntityID(), player.getMoveSpeed());
-                GraphicsEngine.bindAnimation(player.getEntityID(), player.getAnimations().get(Player.MoveDirection.DOWN.name()));
-            }
-        }),
-        GoLeft(new Event() {
-            @Override
-            public void run() {
-
-                PhysicsEngine.goLeft(player.getEntityID(), player.getMoveSpeed());
-                GraphicsEngine.bindAnimation(player.getEntityID(), player.getAnimations().get(Player.MoveDirection.LEFT.name()));
-            }
-        }),
-        GoRight(new Event() {
-            @Override
-            public void run() {
-
-                PhysicsEngine.goRight(player.getEntityID(), player.getMoveSpeed());
-                GraphicsEngine.bindAnimation(player.getEntityID(), player.getAnimations().get(Player.MoveDirection.RIGHT.name()));
-            }
-        }),
-
-        ResizePlayer(new Event() {
-            @Override
-            public void run() {
-                //System.out.println("coucoucocucoucocu");
-                GraphicsEngine.resize(player.getEntityID(), 60, 60);
-            }
-        });
-
-        Event event;
-        EVENTS(Event event) {
-            this.event = event;
-        }
-
-        Event getEvent() {return this.event;}
+    /**
+     * Constructeur
+     */
+    public Gameplay() {
+        initGameplay();
     }
 
-    public static void main(String[] args) {
+    private void initGameplay() {
+        //Activation des entrées / sorties clavier
+        IOEngine.enableKeyboardIO();
 
+        //Joueur
+        player = new Player(30, 30, 2, textures, 1, 3);
 
+        //Id des murs
+        ArrayList<Integer> walls = new ArrayList<>();
 
-        /*Event goDown= new Event() {
-            @Override
-            public void run() {
-
-                PhysicsEngine.goDown(player.getEntityID(), player.getMoveSpeed());
-                GraphicsEngine.bindAnimation(player.getEntityID(), player.getAnimations().get(Player.MoveDirection.DOWN.name()));
+        //Génération des murs
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 6; j++) {
+                int wall = KernelEngine.generateEntity();
+                walls.add(wall);
+                PhysicsEngine.move(wall,30 + (30*j),100 + (60*i));
+                GraphicsEngine.resize(wall,30,30);
+                GraphicsEngine.bindColor(wall,0,0,255);
+                PhysicsEngine.addCollisions(player.getEntityID(), wall);
             }
-        };
+        }
+        int wall = KernelEngine.generateEntity();
+        walls.add(wall);
+        PhysicsEngine.move(wall,0,160);
+        GraphicsEngine.resize(wall,30,30);
+        GraphicsEngine.bindColor(wall,0,0,255);
+        PhysicsEngine.addCollisions(player.getEntityID(), wall);
 
-        Event goLeft= new Event() {
-            @Override
-            public void run() {
+        //Id des boules
+        ArrayList<Integer> balls = new ArrayList<>();
 
-                PhysicsEngine.goLeft(player.getEntityID(), player.getMoveSpeed());
-                GraphicsEngine.bindAnimation(player.getEntityID(), player.getAnimations().get(Player.MoveDirection.LEFT.name()));
-            }
-        };
+        //Génération des boules
+        for (int i = 0; i < 6; i++) {
+            int ball = KernelEngine.generateEntity();
+            balls.add(ball);
+            PhysicsEngine.move(ball,30 + (30*i),130);
+            GraphicsEngine.resize(ball,30,30);
+            GraphicsEngine.bindTexture(ball,textures,10,2);
+            KernelEngine.addEvent("eraseBall" + i, new Event() {
+                @Override
+                public void run() { GraphicsEngine.erase(ball); }
+            });
+            PhysicsEngine.bindEventOnSameLocation(player.getEntityID(), ball, "eraseBall" + i);
+        }
 
-        Event goRight= new Event() {
-            @Override
-            public void run() {
-
-                PhysicsEngine.goRight(player.getEntityID(), player.getMoveSpeed());
-                GraphicsEngine.bindAnimation(player.getEntityID(), player.getAnimations().get(Player.MoveDirection.RIGHT.name()));
-            }
-        };*/
-
-
-        KernelEngine.addEvent("GoUp", EVENTS.GoUp.getEvent());
-        IOEngine.bindEventOnLastKey(KeyEvent.VK_UP, "GoUp");
-
-        KernelEngine.addEvent("GoDown", EVENTS.GoDown.getEvent());
-        IOEngine.bindEventOnLastKey(KeyEvent.VK_DOWN, "GoDown");
-
-        KernelEngine.addEvent("GoRight", EVENTS.GoRight.getEvent());
-        IOEngine.bindEventOnLastKey(KeyEvent.VK_RIGHT, "GoRight");
-
-        KernelEngine.addEvent("GoLeft", EVENTS.GoLeft.getEvent());
-        IOEngine.bindEventOnLastKey(KeyEvent.VK_LEFT, "GoLeft");
-
-
-        //Objet simple
-        int object = KernelEngine.generateEntity();
-        PhysicsEngine.move(object,135,135);
-        GraphicsEngine.resize(object,30,30);
-        GraphicsEngine.bindColor(object,255,0,0);
-
-
-        //Ajout des collisions
-        PhysicsEngine.addCollisions(player.getEntityID(), object);
+        //Ajout des collisions de la scène
         PhysicsEngine.addBoundLimits(player.getEntityID(), 0,0,300,300);
 
         //Scène principale
         int mainScene = GraphicsEngine.generateScene(300, 300);
         GraphicsEngine.setSceneBackgroundColor(mainScene,0,0,0);
         GraphicsEngine.bindScene(mainScene);
-        GraphicsEngine.addToCurrentScene(object);
+        for (Integer id : walls)
+            GraphicsEngine.addToCurrentScene(id);
+        for (Integer id : balls)
+            GraphicsEngine.addToCurrentScene(id);
         GraphicsEngine.addToCurrentScene(player.getEntityID());
 
         //Démarrage du jeu
         KernelEngine.start();
+    }
+
+    public static int getTexturesFile() {
+        return textures;
     }
 }
