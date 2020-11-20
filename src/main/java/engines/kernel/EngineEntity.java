e6976973e3136d03749857754841a4dcb09353df
Quentin Pla <quentin-pla@hotmail.fr>
2020-11-20 21:11:37 +0100

    Suppression des attributs globaux Entity

--- src/main/java/engines/kernel/EngineEntity.java
+++ src/main/java/engines/kernel/EngineEntity.java
@@ -12,39 +12,13 @@ public abstract class EngineEntity {
     /**
      * Constructeur vide
      */
-    public EngineEntity() {}
+    protected EngineEntity() {}
 
     /**
      * Constructeur
      * @param parent parent
      */
-    public EngineEntity(Entity parent) {
+    protected EngineEntity(Entity parent) {
         this.parent = parent;
     }
-
-    // GETTERS & SETTERS //
-
-    public Entity getParent() { return parent; }
-
-    public void setParent(Entity parent) { this.parent = parent; }
-
-    public int getId() { return parent.getId(); }
-
-    public void setId(int id) { parent.setId(id); }
-
-    public int getX() { return parent.getX(); }
-
-    public void setX(int x) { parent.setX(x); }
-
-    public int getY() { return parent.getY(); }
-
-    public void setY(int y) { parent.setY(y); }
-
-    public int getHeight() { return parent.getHeight(); }
-
-    public void setHeight(int height) { parent.setHeight(height); }
-
-    public int getWidth() { return parent.getWidth(); }
-
-    public void setWidth(int width) { parent.setWidth(width); }
 }
--- src/main/java/engines/kernel/Entity.java
+++ src/main/java/engines/kernel/Entity.java
@@ -17,26 +17,6 @@ public class Entity {
     protected int id;
 
     /**
-     * Position x
-     */
-    protected int x;
-
-    /**
-     * Position y
-     */
-    protected int y;
-
-    /**
-     * Hauteur
-     */
-    protected int height;
-
-    /**
-     * Largeur
-     */
-    protected int width;
-
-    /**
      * Entité graphique
      */
     protected GraphicEntity graphicEntity;
@@ -56,10 +36,7 @@ public class Entity {
      */
     protected Entity() {
         id = KernelEngine.generateNewID();
-        x = 0;
-        y = 0;
-        height = 0;
-        width = 0;
+        System.out.println(id);
         graphicEntity = GraphicsEngine.getInstance().createEntity(this);
         physicEntity = PhysicsEngine.getInstance().createEntity(this);
         ioEntity = IOEngine.getInstance().createEntity(this);
@@ -71,19 +48,9 @@ public class Entity {
 
     public void setId(int id) { this.id = id; }
 
-    public int getX() { return x; }
+    public GraphicEntity getGraphicEntity() { return graphicEntity; }
 
-    public void setX(int x) { this.x = x; }
+    public PhysicEntity getPhysicEntity() { return physicEntity; }
 
-    public int getY() { return y; }
-
-    public void setY(int y) { this.y = y; }
-
-    public int getHeight() { return height; }
-
-    public void setHeight(int height) { this.height = height; }
-
-    public int getWidth() { return width; }
-
-    public void setWidth(int width) { this.width = width; }
+    public IOEntity getIoEntity() { return ioEntity; }
 }
