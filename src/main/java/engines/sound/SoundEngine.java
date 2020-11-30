5733b221258be70fb72bb8032229cba21b6d672e
axel-ribes <axel.ribes@etu.univ-amu.fr>
2020-11-30 17:19:18 +0100

    ajout du controle du volume

--- src/main/java/engines/sound/SoundEngine.java
+++ src/main/java/engines/sound/SoundEngine.java
@@ -44,7 +44,7 @@ public class SoundEngine {
      * @param path chamin
      * @param soundName nom du son
      */
-    public Clip loadSound(String path,String soundName) {
+    public void loadSound(String path, String soundName) {
         Clip clip = null;
         try {
             clip = AudioSystem.getClip();
@@ -56,7 +56,6 @@ public class SoundEngine {
             e.printStackTrace();
         }
         sounds.put(soundName,clip);
-        return clip;
     }
 
     /**
@@ -118,7 +117,37 @@ public class SoundEngine {
         return playingSounds.contains(sounds.get(name));
     }
 
+    /**
+     * récupérer tous les souns
+     * @return
+     */
     public Map<String, Clip> getSounds() {
         return sounds;
     }
+
+    /**
+     * récupérer le volume actuel
+     * @param name
+     * @return
+     */
+    public float getVolume(String name) {
+        Clip sound = sounds.get(name);
+        FloatControl gainControl = (FloatControl) sound.getControl(FloatControl.Type.MASTER_GAIN);
+        return (float) Math.pow(10f, gainControl.getValue() / 20f);
+    }
+
+    /**
+     * mettre un nouveau volume
+     * @param name
+     * @param volume
+     */
+    public void setVolume(String name,float volume) {
+        Clip sound = sounds.get(name);
+        if (volume < 0f || volume > 1f)
+            throw new IllegalArgumentException("Volume not valid: " + volume);
+        FloatControl gainControl = (FloatControl) sound.getControl(FloatControl.Type.MASTER_GAIN);
+        gainControl.setValue(20f * (float) Math.log10(volume));
+    }
+
+
 }
